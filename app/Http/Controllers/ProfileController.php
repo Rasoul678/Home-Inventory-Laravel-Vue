2c36f2acb5051db3980b80fab90b6ad51f77dcd2
Rasoul678 <h.rostami.r@gmail.com>
2020-09-02 14:44:20 +0430

    Remove Previous Avatar

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -43,14 +43,11 @@ class ProfileController extends Controller
         }else
         {
             if ($image = request()->file('avatar')) {
-//                if(isset($user->avatar_url)){
-//                    $user->update([
-//                        'avatar_url'=> '',
-//                        'avatar_public_id'=> ''
-//                    ]);
-//                }
+                if(isset($user->avatar_url)){
+                    Cloudder::destroyImage($user->avatar_public_id);
+                }
                 $image_path = $image->getRealPath();
-                Cloudder::upload($image_path,  now() . $user->name, [
+                Cloudder::upload($image_path,  now(). '_' . $user->name, [
                     "folder" => "inventory-laravel/avatars/",
                     ]);
                 $publicId = Cloudder::getPublicId();
